634f2e6f4aa3d00285879d359d7fe91f3cab9d93
KerenC <46246176+KerenC91@users.noreply.github.com>
2019-01-20 13:38:19 +0200

    connecting firebase pjt
    connecting firebase pjt

--- functions/index.js
+++ functions/index.js
@@ -1,23 +1,22 @@
-
-'use strict';
-
+// The Cloud Functions for Firebase SDK to create Cloud Functions and setup triggers.
 const functions = require('firebase-functions');
 
+// The Firebase Admin SDK to access the Firebase Realtime Database.
 const admin = require('firebase-admin');
 admin.initializeApp();
 
-
 // Take the text parameter passed to this HTTP endpoint and insert it into the
 // Realtime Database under the path /messages/:pushId/original
-
-exports.addMessage = functions.https.onRequest(async (req, res) => {
+exports.addMessage = functions.https.onRequest((req, res) => {
+  // Grab the text parameter.
   const original = req.query.text;
-  // [START adminSdkPush]
   // Push the new message into the Realtime Database using the Firebase Admin SDK.
-  const snapshot = await admin.database().ref('/messages').push({original: original});
-  // Redirect with 303 SEE OTHER to the URL of the pushed object in the Firebase console.
-  res.redirect(303, snapshot.ref.toString());
+  return admin.database().ref('/messages').push({original: original}).then((snapshot) => {
+    // Redirect with 303 SEE OTHER to the URL of the pushed object in the Firebase console.
+    return res.redirect(303, snapshot.ref.toString());
+  });
 });
+
 // Listens for new messages added to /messages/:pushId/original and creates an
 // uppercase version of the message to /messages/:pushId/uppercase
 exports.makeUppercase = functions.database.ref('/messages/{pushId}/original')
